a7279826b957e422db5e8d07dfe0af6ce38a3a54
0yassin <yassinz8275@gmail.com>
2026-07-24 18:59:35 +0100

    implemented websocket connections and device list broadcast

--- server/server.go
+++ server/server.go
@@ -5,23 +5,64 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"sync"
 
+	"github.com/gofiber/contrib/v3/websocket"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/google/uuid"
 )
+
+type Client struct {
+	ID   string
+	Name string
+	Conn *websocket.Conn
+}
+
+var clients = make(map[string]*Client)
+var clientsMu sync.RWMutex
 
 func main() {
 	port := "3000"
 	app := fiber.New()
 	app.Use(cors.New())
 
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		var id string = uuid.New().String()
+		defaultName := fmt.Sprintf("device-%d", len(clients))
+		clientsMu.Lock()
+		clients[id] = &Client{
+			ID:   id,
+			Name: c.Query("name", defaultName),
+			Conn: c,
+		}
+		clientsMu.Unlock()
+		broadcastdevices()
+		fmt.Printf("A new device just connected, ID: %s \n", id)
+		for {
+			msgType, msg, err := c.ReadMessage()
+			if err != nil {
+				clientsMu.Lock()
+				delete(clients, id)
+				clientsMu.Unlock()
+				broadcastdevices()
+				break
+			}
+			fmt.Printf("Received: %s %s \n", msg, strconv.Itoa(msgType))
+		}
+	}))
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendFile("dist/main.html")
 	})
 
 	app.Post("/upload", func(c fiber.Ctx) error {
 		fmt.Println("req recieved from", c.IP())
+		id := c.FormValue("tarID")
 		file, err := c.FormFile("document")
+		tar := c.FormValue("target")
+		filename := c.FormValue("filename")
+
 		if err != nil {
 			return fiber.NewError(fiber.StatusBadRequest, "No file included")
 		}
@@ -29,7 +70,21 @@ func main() {
 		if err != nil {
 			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 		}
-		fmt.Println("/upload sucess ", file.Filename)
+		clientsMu.Lock()
+		target, exists := clients[id]
+		clientsMu.Unlock()
+		if exists {
+			target.Conn.WriteJSON(fiber.Map{
+				"event":    "incoming_file",
+				"filename": filename,
+				"sender":   c.IP(),
+			})
+			fmt.Printf(" Alerted %s, filename: %s", target.ID, filename)
+		} else {
+			fmt.Printf("Device not connected")
+		}
+
+		fmt.Printf(" /upload sucess to %s - %s \n", tar, filename)
 
 		return c.JSON(fiber.Map{
 			"status":   200,
@@ -58,4 +113,25 @@ func getaddr() string {
 	}
 	defer conn.Close()
 	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
+
+func broadcastdevices() {
+	clientsMu.RLock()
+	defer clientsMu.RUnlock()
+	var activeusers []fiber.Map
+	for _, client := range clients {
+		activeusers = append(activeusers, fiber.Map{
+			"id":   client.ID,
+			"name": client.Name,
+		})
+	}
+	for _, client := range clients {
+		err := client.Conn.WriteJSON(fiber.Map{
+			"event": "devices_update",
+			"users": activeusers,
+		})
+		if err != nil {
+			fmt.Println("Error sending to", client.Name)
+		}
+	}
 }
